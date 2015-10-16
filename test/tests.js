fe9e673a32471445f3a515e54d169b42c658e51c
JavierPestanoRon <Javipestano@gmail.com>
2015-10-16 11:31:09 +0100

    delete

--- test/tests.js
+++ test/tests.js
@@ -42,10 +42,8 @@ describe("Test Funcionales", function(){
     });*/
     
     it("Prueba completa", function() {
-              window.onload = function() {
-            original.value = "32F";
-            expect(calculate()).to.equal(0);
-              }
+        original.value = "32F";
+        expect(calculate()).to.equal(0);
     });
     
     it("32,0576F === 0.032C", function() {
